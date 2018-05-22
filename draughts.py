590608ba3c40b019c78b9a9ca5db26f435b98eca
xavier bugg <bugg.xavier@gmail.com>
2018-05-22 11:49:05 +0000

    Test

--- draughts.py
+++ draughts.py
@@ -1,2 +1,7 @@
-import flask
+from flask import Flask, render_template, redirect, url_for, request, make_response, jsonify
 import records
+
+@app.route('/')
+def index():
+    # Render the start page
+    return render_template('index.html')
