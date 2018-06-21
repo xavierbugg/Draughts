8631bbb4e3aac1805adeb0a53717357d81ad1953
xavier bugg <bugg.xavier@gmail.com>
2018-06-21 08:42:53 +0000

    Update

--- draughts.py
+++ draughts.py
@@ -1,6 +1,6 @@
 from flask import Flask, render_template, redirect, url_for, request, make_response, jsonify
 import records
-
+app = Flask(__name__)
 @app.route('/')
 def index():
     # Render the start page
